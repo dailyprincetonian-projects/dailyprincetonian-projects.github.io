60f61df58acaa75412ca27e956c0170f13e28ace
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-16 12:44:43 -0700

    Update map.js

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ d3.json("https://raw.githubusercontent.com/princetoncoronavirus/princetoncoronav
 
 //Width and height of map
 var width = 300
-var height = 530
+var height = 550
 	
 // var projection = d3.geo.mercator()
 //   .scale(5758)
