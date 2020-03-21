e9be2a27861ba60167982d970e74e2e5136777f9
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-21 10:49:15 -0700

    Update casesovertime.js

--- casesovertime.js
+++ casesovertime.js
@@ -81,7 +81,7 @@ d3.csv("prices.csv")
 			.interpolate("cardinal");
 		
 		
-		var svg = d3.select("body").append("svg").attr("id","svg").attr("height","100%").attr("width","100%");
+		var svg = d3.select("#chart").append("svg").attr("id","svg").attr("height","100%").attr("width","100%");
 		var chartGroup = svg.append("g").attr("class","chartGroup").attr("transform","translate("+xNudge+","+yNudge+")");
 		
 		chartGroup.append("path")
