d6956b219795c82f766d15fd76f86e96a752c627
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-21 12:25:06 -0700

    Update casesovertime.js

--- casesovertime.js
+++ casesovertime.js
@@ -26,9 +26,11 @@ d3.csv("casesovertime.csv")
 
 
 		var y = d3.scale.linear()
+					.domain([0,max])
 					.range([height,0]);
 		
 		var x = d3.time.scale()
+					.domain([minDate,maxDate])
 					.range([0,width]);
 		
 		var yAxis = d3.svg.axis()
