07544fa124aa2c7dd3f5baf52032d1a708f167ca
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-20 19:16:06 -0700

    Update testmap.js

--- testmap.js
+++ testmap.js
@@ -76,8 +76,8 @@ svg.selectAll("circle")
       	   .duration(200)      
            .style("opacity", .9);      
            div.text(d.county + ' cases: ' + d.cases)
-           .style("left", (projection([d.lon, d.lat])[0]) + "px")     
-           .style("top", (projection([d.lon, d.lat])[1] - 28) + "px");    
+           .style("left", (projection([d.lon, d.lat])[0]))     
+           .style("top", (projection([d.lon, d.lat])[1]));    
 	})   
 
     // fade out tooltip on mouse out               
