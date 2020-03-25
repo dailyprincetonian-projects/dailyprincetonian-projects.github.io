1d1c92d3d6f169744b610d0823aeb8e78efd452b
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-24 18:02:35 -0700

    Update map.js

--- map.js
+++ map.js
@@ -14,7 +14,7 @@ var projection = d3.geo.mercator()
   .scale(8000)
   .center([-74.71, 40.31])
   .translate([width/2, height/2]);
-	
+
 var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
 	.projection(projection);
 
@@ -36,12 +36,11 @@ svg.selectAll("path")
 	.enter()
 	.append("path")
 	.attr("d", path)
-	.style("stroke", "rgb(175,175,175)")
-	.style("stroke-width", "0.5")
-	.style("fill", "rgb(245,245,245)")
+	.style("stroke", "rgb(255,255,255)")
+	.style("stroke-width", "4.5")
+	.style("fill", "rgb(240,240,240)")
 	
-d3.csv("cases.csv", function(data) {
-
+d3.csv("cities.csv", function(data) {
 svg.selectAll("circle")
 	.data(data)
 	.enter()
@@ -53,12 +52,48 @@ svg.selectAll("circle")
 		return projection([d.lon, d.lat])[1];
 	})
 	.attr("r", function(d) {
-		return Math.sqrt(d.cases);
+		return 3;
+	})
+		.style("fill", "rgb(0,0,0)")	
+		.style("fill-opacity", 1.0)
+		.style("stroke", "rgb(0,0,0)")
+		.style("stroke-opacity", 1.0)
+	.append("title").text(d=>d.city);
+});
+
+d3.csv("cities.csv", function(data) {
+svg.selectAll("text")
+	.data(data)
+	.enter()
+	.append("text")
+  	.attr("x", function(d) { return projection([d.lon, d.lat])[0]; })
+ 	.attr("y", function(d) { return projection([d.lon, d.lat])[1] - 5; })
+	.attr("text-anchor", d=>d.anchor)
+  	.text(d=>d.city)
+});
+	
+	
+	
+d3.csv("cases.csv", function(data) {
+
+
+svg.selectAll("g")
+	.data(data)
+	.enter()
+	.append("circle")
+	.attr("cx", function(d) {
+		return projection([d.lon, d.lat])[0];
+	})
+	.attr("cy", function(d) {
+		return projection([d.lon, d.lat])[1];
+	})
+	.attr("r", function(d) {
+		return Math.sqrt(d.cases) * 1.5;
 	})
 		.style("fill", "#ff8f00")	
-		.style("fill-opacity", 0.3)
+		.style("fill-opacity", 0.5)
 		.style("stroke", "#ff8f00")
-		.style("stroke-opacity", 1.0)
+		.style("stroke-opacity", 0.5)
 
 	.on("mouseover", function(d) {      
     	div.transition()        
@@ -66,7 +101,7 @@ svg.selectAll("circle")
            .style("opacity", .9);      
            div.text(d.county + ' cases: ' + d.cases)
            .style("left", (d3.event.pageX) + "px")     
-           .style("top", (d3.event.pageY - 28) + "px");    
+           .style("top", (d3.event.pageY - 28) + "px");   
 	})   
 
     // fade out tooltip on mouse out               
@@ -77,3 +112,67 @@ svg.selectAll("circle")
     });
 });
 }); 
+
+	// OLD CODE
+// var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+// 	.projection(projection);
+
+	
+// //Create SVG element and append map to the SVG
+// var svg = d3.selectAll("#map")
+// 			.append("svg")
+// 			.attr("width", width)
+// 			.attr("height", height);
+        
+// // Append Div for tooltip to SVG
+// var div = d3.selectAll("#map")
+// 		    .append("div")   
+//     		.attr("class", "tooltip")               
+//     		.style("opacity", 0);
+// // Bind the data to the SVG and create one path per GeoJSON feature
+// svg.selectAll("path")
+// 	.data(json.features)
+// 	.enter()
+// 	.append("path")
+// 	.attr("d", path)
+// 	.style("stroke", "rgb(175,175,175)")
+// 	.style("stroke-width", "0.5")
+// 	.style("fill", "rgb(245,245,245)")
+	
+// d3.csv("cases.csv", function(data) {
+
+// svg.selectAll("circle")
+// 	.data(data)
+// 	.enter()
+// 	.append("circle")
+// 	.attr("cx", function(d) {
+// 		return projection([d.lon, d.lat])[0];
+// 	})
+// 	.attr("cy", function(d) {
+// 		return projection([d.lon, d.lat])[1];
+// 	})
+// 	.attr("r", function(d) {
+// 		return Math.sqrt(d.cases);
+// 	})
+// 		.style("fill", "#ff8f00")	
+// 		.style("fill-opacity", 0.3)
+// 		.style("stroke", "#ff8f00")
+// 		.style("stroke-opacity", 1.0)
+
+// 	.on("mouseover", function(d) {      
+//     	div.transition()        
+//       	   .duration(200)      
+//            .style("opacity", .9);      
+//            div.text(d.county + ' cases: ' + d.cases)
+//            .style("left", (d3.event.pageX) + "px")     
+//            .style("top", (d3.event.pageY - 28) + "px");    
+// 	})   
+
+//     // fade out tooltip on mouse out               
+//     .on("mouseout", function(d) {       
+//         div.transition()        
+//            .duration(500)      
+//            .style("opacity", 0);   
+//     });
+// });
+// }); 
