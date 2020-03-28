2afc4f664f41dd2220927470d7ac0da77e6da760
dailyprincetonian-projects <62261834+dailyprincetonian-projects@users.noreply.github.com>
2020-03-28 09:09:09 -0700

    Update map.js

--- map.js
+++ map.js
@@ -88,7 +88,7 @@ svg.selectAll("g")
 		return projection([d.lon, d.lat])[1];
 	})
 	.attr("r", function(d) {
-		return Math.sqrt(d.cases) * 0.75;
+		return Math.sqrt(d.cases) * 0.75; //circle radius
 	})
 		.style("fill", "#ff8f00")	
 		.style("fill-opacity", 0.5)
